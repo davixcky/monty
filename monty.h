4f5621fc9f38b2accc8a0f87284a5a6213cebf41
David Orozco <dvdizcky@gmail.com>
2020-05-14 08:14:23 -0500

    Implementations added

--- monty.h
+++ monty.h
@@ -60,6 +60,7 @@ void push(stack_t **stack, unsigned int line_number);
 void pop(stack_t **head, unsigned int line_number);
 void pint(stack_t **head, unsigned int line_number);
 void swap(stack_t **head, unsigned int line_number);
+void add(stack_t **head, unsigned int line_number);
 
 /* helpers.c */
 int to_number(int *number);
