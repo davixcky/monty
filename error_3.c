9aba83b9478b01ba4492ec9c3961972f2df08a01
hector vinicio lopez molinares <hectorvmlopez@gmail.com>
2020-05-21 16:00:29 -0500

    fiX

--- error_3.c
+++ error_3.c
@@ -50,3 +50,13 @@ void pchar_error_2(void)
 	fprintf(stderr, "L%d: can't pchar, value out of range\n", info.line_number);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ *pchar_error - pop error
+ *Return: Nothing
+ */
+void pchar_error(void)
+{
+	fprintf(stderr, "L%d: can't pchar, value out of range\n", info.line_number);
+	exit(EXIT_FAILURE);
+}
